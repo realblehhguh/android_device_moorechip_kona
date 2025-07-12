ac9c014a11dfba19abb2282f58d1ddc85521385b
realblehhguh <iamhappyandfree@proton.me>
2025-07-12 17:50:51 -0400

    clean up

---
+++ device.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/moorechip/kona
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.kona
+
+# Boot control HAL shared libraries
+PRODUCT_PACKAGES += \
+    libgptutils \
+    libz \
+    libcutils
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
+
+# Crypto and Decryption
+PRODUCT_PACKAGES += \
+    qseecomd \
+    keymaster_mobicore \
+    android.hardware.keymaster@4.0-impl \
+    android.hardware.keymaster@4.0-service \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
+
+
---
+++ vendorsetup.sh
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Lunch combos are now defined in AndroidProducts.mk
+# This file is kept for compatibility but functionality moved to AndroidProducts.mk
