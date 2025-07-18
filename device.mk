f58a90bfe433c7b3afd061352e47a952a660367b
realblehhguh <101596581+realblehhguh@users.noreply.github.com>
2025-07-18 16:13:34 -0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -11,7 +11,8 @@ AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+    POSTINSTALL_OPTIONAL_system=true \
+    ENABLE_VIRTUAL_AB := true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
