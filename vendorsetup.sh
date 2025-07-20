e06b2adea86fc9f44b422ad12fe7418d58f0946f
realblehhguh <iamhappyandfree@proton.me>
2025-07-20 16:37:48 -0400

    clean

--- vendorsetup.sh
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Lunch combos are now defined in AndroidProducts.mk
-# This file is kept for compatibility but functionality moved to AndroidProducts.mk
