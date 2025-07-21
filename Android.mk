6878a734013ec7ace5cc532bc97e2252b62f77a3
realblehhguh <101596581+realblehhguh@users.noreply.github.com>
2025-07-21 03:12:35 -0400

    revert

--- Android.mk
+++ Android.mk
@@ -7,9 +7,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
-    $(LOCAL_PATH)/init.recovery.qcom_decrypt.rc:root/init.recovery.qcom_decrypt.rc
 
 ifeq ($(TARGET_DEVICE),kona)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
